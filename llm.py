3e5b70863a3afc3b0356e6397225bbcf0f75f4d1
Srikrishna-Kumarasamy <sk11595@nyu.edu>
2025-01-26 18:57:25 -0500

    Added embeddings and temperature for openai model

--- llm.py
+++ llm.py
@@ -1,6 +1,7 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
@@ -9,13 +10,18 @@ class LLM:
     def __init__(self, model="gpt-4o"):
         self.model = model
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        self.embeddings_model = SentenceTransformer('all-Mpnet-base-v2')
 
-    def answer(self, prompt):
+    def answer(self, prompt, temperature=0):
         response = self.client.chat.completions.create(
             messages=[{
                 "role": "user",
                 "content": prompt,
             }],
             model=self.model,
+            temperature=temperature
         )
         return response.choices[0].message.content
+
+    def get_embeddings(self, text):
+        return self.embeddings_model.encode(text)
